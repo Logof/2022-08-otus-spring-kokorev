bff69090fb167e864cfedd14a587fb759e2b703a
Logof <Logof2008@mail.ru>
2022-10-06 18:01:27 +0500

    fix замечаний

--- hw-03/src/main/java/ru/otus/homework/resolver/FilePathResolverImpl.java
+++ hw-03/src/main/java/ru/otus/homework/resolver/FilePathResolverImpl.java
@@ -5,17 +5,15 @@ import org.springframework.stereotype.Component;
 import ru.otus.homework.service.LocaleProviderService;
 
 @Component
-public class FilePathRecolverImpl implements FilePathResolver {
+public class FilePathResolverImpl implements FilePathResolver {
 
     private final String filePath;
 
-    private final LocaleProviderService localeProviderService;
-
-    public FilePathRecolverImpl(@Value("${application.file.location}") String fileLocation,
+    public FilePathResolverImpl(@Value("${application.file.location}") String fileLocation,
                                 LocaleProviderService localeProviderService) {
         this.filePath = fileLocation.replaceFirst("\\$(\\w+)", localeProviderService.getLocale().getLanguage());
-        this.localeProviderService = localeProviderService;
     }
+
 
     @Override
     public String getFilePath() {
