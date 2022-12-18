70ef2d43ec7b52ef466180638d2a99a6f0947c9b
Logof <Logof2008@mail.ru>
2022-12-19 00:49:45 +0500

    UUID -> String

--- hw-08-mongodb/src/main/java/ru/otus/homework/hw08/repository/BookRepository.java
+++ hw-08-mongodb/src/main/java/ru/otus/homework/hw08/repository/BookRepository.java
@@ -6,19 +6,14 @@ import ru.otus.homework.hw08.entity.Book;
 import ru.otus.homework.hw08.entity.Genre;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BookRepository extends MongoRepository<Book, String>, BookMongodbRepository {
 
     List<Book> findAllByGenres(Genre genre);
 
-    Optional<Book> findOneByGenres(Genre genre);
-
     boolean existsByGenres(Genre genre);
 
     List<Book> findAllByAuthors(Author author);
-
-    Optional<Book> findOneByAuthors(Author author);
 
     boolean existsByAuthors(Author author);
 
